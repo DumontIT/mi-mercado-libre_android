c6b5570699437771bdd1749a7453e1848eb2e15f
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-05-18 18:40:41 -0300

    Add basic test using JUnit 4 (from Intellij IDEA).

---
+++ src/test/java/com/nbempire/pp/service/ProductServiceTest.java
@@ -0,0 +1,14 @@
+package com.nbempire.pp.service;
+
+import org.junit.Test;
+
+public class ProductServiceTest {
+
+    /**
+     * Test method for findAveragePrice().
+     */
+    @Test
+    public void testfindAveragePrice() throws Exception {
+
+    }
+}
