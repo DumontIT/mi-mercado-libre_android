2986f4a8ae512def36f276dbbd16b7b380b9de35
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-05-20 16:43:08 -0300

    Display a list of currently installed apps in my queries section.

--- Super ML (Mobile App)/src/main/java/com/nbempire/superml/component/activity/HomeActivity.java
+++ Super ML (Mobile App)/src/main/java/com/nbempire/superml/component/activity/HomeActivity.java
@@ -1,11 +1,20 @@
 package com.nbempire.superml.component.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
@@ -15,13 +24,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import com.nbempire.superml.R;
+import com.nbempire.superml.domain.Category;
 import com.nbempire.superml.domain.Product;
 import com.nbempire.superml.service.ProductService;
 import com.nbempire.superml.service.impl.ProductServiceImpl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 
@@ -200,18 +214,57 @@ public class HomeActivity extends ActionBarActivity implements ActionBar.TabList
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             int fragmentLayoutId = R.layout.fragment_average_price;
+
             switch (sectionNumer) {
                 case 2:
                     fragmentLayoutId = R.layout.fragment_my_queries;
             }
+            View createdLayoutView = inflater.inflate(fragmentLayoutId, container, false);
 
+            switch (sectionNumer) {
+                case 1:
+                    onCreateViewForAveragePriceFragment(createdLayoutView);
+                    break;
+                case 2:
+                    onCreateViewForMyQueriesFragment(createdLayoutView);
+            }
+
+            return createdLayoutView;
+        }
+
+        private void onCreateViewForAveragePriceFragment(View container) {
             query = (EditText) container.findViewById(R.id.homeSearchQuery);
             averagePrice = (TextView) container.findViewById(R.id.homeAveragePrice);
             minimumPrice = (TextView) container.findViewById(R.id.homeMinimumPrice);
             maximumPrice = (TextView) container.findViewById(R.id.homeMaximumPrice);
             moneySymbol = (TextView) container.findViewById(R.id.homeMoneySymbol);
+        }
 
-            return inflater.inflate(fragmentLayoutId, container, false);
+        private void onCreateViewForMyQueriesFragment(final View container) {
+            final ListView categories = (ListView) container.findViewById(R.id.listView);
+
+            final CategoryAdapter mAdapter = new CategoryAdapter(container.getContext());
+            categories.setAdapter(mAdapter);
+
+            // Prepare the loader.  Either re-connect with an existing one, or start a new one.
+            getLoaderManager().initLoader(0, null, new LoaderManager.LoaderCallbacks<List<Category>>() {
+                @Override
+                public Loader<List<Category>> onCreateLoader(int id, Bundle args) {
+                    return new CategoryListLoader(container.getContext());
+                }
+
+                @Override
+                public void onLoadFinished(Loader<List<Category>> loader, List<Category> data) {
+                    // Set the new data in the adapter.
+                    mAdapter.setData(data);
+                }
+
+                @Override
+                public void onLoaderReset(Loader<List<Category>> loader) {
+                    // Clear the data in the adapter.
+                    mAdapter.setData(null);
+                }
+            });
         }
     }
 
@@ -243,6 +296,216 @@ public class HomeActivity extends ActionBarActivity implements ActionBar.TabList
             maximumPrice.setText(String.valueOf(result.getMaximumPrice()));
 
             updateViewsVisibility(View.VISIBLE, new View[]{averagePrice, minimumPrice, maximumPrice, moneySymbol});
+        }
+    }
+
+    public static class CategoryAdapter extends ArrayAdapter<Category> {
+
+        private final LayoutInflater layoutInflater;
+
+        public CategoryAdapter(Context context) {
+            super(context, android.R.layout.simple_list_item_1);
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public void setData(List<Category> data) {
+            clear();
+            if (data != null) {
+                for (Category eachCategory : data) {
+                    add(eachCategory);
+                }
+            }
+        }
+
+        /**
+         * Populate new items in the list.
+         */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+
+            if (convertView == null) {
+                view = layoutInflater.inflate(R.layout.abc_activity_chooser_view_list_item, parent, false);
+            } else {
+                view = convertView;
+            }
+
+            Category category = getItem(position);
+            ((TextView) view.findViewById(R.id.title)).setText(category.getName());
+
+            return view;
+        }
+    }
+
+    /**
+     * Helper class to look for interesting changes to the installed apps so that the loader can be updated.
+     */
+    public static class PackageIntentReceiver extends BroadcastReceiver {
+
+        final CategoryListLoader mLoader;
+
+        public PackageIntentReceiver(CategoryListLoader loader) {
+            mLoader = loader;
+            IntentFilter filter = new IntentFilter(Intent.ACTION_PACKAGE_ADDED);
+            filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+            filter.addAction(Intent.ACTION_PACKAGE_CHANGED);
+            filter.addDataScheme("package");
+            mLoader.getContext().registerReceiver(this, filter);
+            // Register for events related to sdcard installation.
+            IntentFilter sdFilter = new IntentFilter();
+            sdFilter.addAction(Intent.ACTION_EXTERNAL_APPLICATIONS_AVAILABLE);
+            sdFilter.addAction(Intent.ACTION_EXTERNAL_APPLICATIONS_UNAVAILABLE);
+            mLoader.getContext().registerReceiver(this, sdFilter);
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Tell the loader about the change.
+            mLoader.onContentChanged();
+        }
+    }
+
+    /**
+     * A custom Loader that loads all of the installed applications.
+     */
+    public static class CategoryListLoader extends AsyncTaskLoader<List<Category>> {
+
+        private final PackageManager packageManager;
+
+        private List<Category> categories;
+
+        private PackageIntentReceiver packageIntentReceiver;
+
+        public CategoryListLoader(Context context) {
+            super(context);
+
+            // Retrieve the package manager for later use; note we don't use 'context' directly but instead the save global application context returned by getContext().
+            packageManager = getContext().getPackageManager();
+        }
+
+        /**
+         * This is where the bulk of our work is done.  This function is called in a background thread and should generate a new set of data to be
+         * published by the loader.
+         */
+        @Override
+        public List<Category> loadInBackground() {
+            // Retrieve all known applications.
+            List<ApplicationInfo> apps = packageManager.getInstalledApplications(
+                    PackageManager.GET_UNINSTALLED_PACKAGES |
+                    PackageManager.GET_DISABLED_COMPONENTS
+            );
+            if (apps == null) {
+                apps = new ArrayList<ApplicationInfo>();
+            }
+
+            // Create corresponding array of categories and load their labels.
+            List<Category> categories = new ArrayList<Category>(apps.size());
+            for (ApplicationInfo app : apps) {
+                if (app.className != null && !app.className.equals("")) {
+                    categories.add(new Category(app.className));
+                }
+            }
+
+            return categories;
+        }
+
+        /**
+         * Called when there is new data to deliver to the client.  The super class will take care of delivering it; the implementation here just adds
+         * a little more logic.
+         */
+        @Override
+        public void deliverResult(List<Category> data) {
+            if (isReset()) {
+                // An async query came in while the loader is stopped.  We don't need the result.
+                if (data != null) {
+                    onReleaseResources(data);
+                }
+            }
+
+            List<Category> oldApps = data;
+            categories = data;
+
+            if (isStarted()) {
+                // If the Loader is currently started, we can immediately deliver its results.
+                super.deliverResult(data);
+            }
+
+            // At this point we can release the resources associated with 'oldApps' if needed; now that the new result is delivered we know that it is no longer in use.
+            if (oldApps != null) {
+                onReleaseResources(oldApps);
+            }
+        }
+
+        /**
+         * Handles a request to start the Loader.
+         */
+        @Override
+        protected void onStartLoading() {
+            if (categories != null) {
+                // If we currently have a result available, deliver it
+                // immediately.
+                deliverResult(categories);
+            }
+
+            // Start watching for changes in the app data.
+            if (packageIntentReceiver == null) {
+                packageIntentReceiver = new PackageIntentReceiver(this);
+            }
+
+            if (takeContentChanged() || categories == null) {
+                // If the data has changed since the last time it was loaded or is not currently available, start a load.
+                forceLoad();
+            }
+        }
+
+        /**
+         * Handles a request to stop the Loader.
+         */
+        @Override
+        protected void onStopLoading() {
+            // Attempt to cancel the current load task if possible.
+            cancelLoad();
+        }
+
+        /**
+         * Handles a request to cancel a load.
+         */
+        @Override
+        public void onCanceled(List<Category> data) {
+            super.onCanceled(data);
+
+            // At this point we can release the resources associated with 'data' if needed.
+            onReleaseResources(data);
+        }
+
+        /**
+         * Handles a request to completely reset the Loader.
+         */
+        @Override
+        protected void onReset() {
+            super.onReset();
+
+            // Ensure the loader is stopped
+            onStopLoading();
+
+            // At this point we can release the resources associated with 'apps' if needed.
+            if (categories != null) {
+                onReleaseResources(categories);
+                categories = null;
+            }
+
+            // Stop monitoring for changes.
+            if (packageIntentReceiver != null) {
+                getContext().unregisterReceiver(packageIntentReceiver);
+                packageIntentReceiver = null;
+            }
+        }
+
+        /**
+         * Helper function to take care of releasing resources associated with an actively loaded data set.
+         */
+        protected void onReleaseResources(List<Category> apps) {
+            // For a simple List<> there is nothing to do.  For something like a Cursor, we would close it here.
         }
     }
 }
