cf3ff46896ab4cdb4b4d304256c1035e9aa54079
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-06-06 23:29:13 -0300

    Send SubscriptionDto.java on POST body when subscribing user to a product.

---
+++ Super ML (Mobile App)/src/main/java/com/nbempire/superml/dto/SubscriptionDto.java
@@ -0,0 +1,18 @@
+package com.nbempire.superml.dto;
+
+/**
+ * TODO : Javadoc for
+ * <p/>
+ * Created on 06/06/14, at 23:14.
+ *
+ * @author Nahuel Barrios <barrios.nahuel@gmail.com>.
+ * @since 1.
+ */
+public class SubscriptionDto {
+
+    private String query;
+
+    public SubscriptionDto(String query) {
+        this.query = query;
+    }
+}
--- Super ML (Mobile App)/src/main/java/com/nbempire/superml/service/impl/UserDaoImplSpring.java
+++ Super ML (Mobile App)/src/main/java/com/nbempire/superml/service/impl/UserDaoImplSpring.java
@@ -4,6 +4,7 @@ import android.util.Log;
 import com.nbempire.superml.MainKeys;
 import com.nbempire.superml.dao.UserDao;
 import com.nbempire.superml.domain.User;
+import com.nbempire.superml.dto.SubscriptionDto;
 import com.nbempire.superml.exception.UnfixableException;
 import org.springframework.http.HttpAuthentication;
 import org.springframework.http.HttpBasicAuthentication;
@@ -13,6 +14,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -38,9 +40,12 @@ public class UserDaoImplSpring implements UserDao {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setAuthorization(authHeader);
 
-        HttpEntity<?> requestEntity = new HttpEntity<Object>(httpHeaders);
+        //  TODO : Unhard-code this...
+        SubscriptionDto dto = new SubscriptionDto("ipod touch");
+        HttpEntity<SubscriptionDto> requestEntity = new HttpEntity<SubscriptionDto>(dto, httpHeaders);
 
         RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new GsonHttpMessageConverter());
         restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
 
         String urlString = String.format(MainKeys.API_HOST + "/users/%s/subscriptions", user.getId());
